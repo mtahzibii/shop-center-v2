16d611b297414ec14abed3915abd68233215d707
Mohammadreza Tahzibi <m.r.tahzibi@gmail.com>
2022-07-16 18:43:08 +0430

    feat: Imported sample data to database

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -9,21 +9,32 @@ const orderSchema = mongoose.Schema(
   },
   orderItems: [
    {
-    qty: { type: Number, required: true },
     name: { type: String, required: true },
+    qty: { type: Number, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
     product: {
      type: mongoose.Schema.Types.ObjectId,
-     require: true,
+     required: true,
      ref: 'Product',
     },
    },
   ],
-  totalPrice: {
-   type: Number,
+  shippingAddress: {
+   address: { type: String, required: true },
+   city: { type: String, required: true },
+   postalCode: { type: String, required: true },
+   country: { type: String, required: true },
+  },
+  paymentMethod: {
+   type: String,
    required: true,
-   default: 0.0,
+  },
+  paymentResult: {
+   id: { type: String },
+   status: { type: String },
+   update_time: { type: String },
+   email_address: { type: String },
   },
   taxPrice: {
    type: Number,
@@ -35,15 +46,10 @@ const orderSchema = mongoose.Schema(
    required: true,
    default: 0.0,
   },
-  paymentMethod: {
-   type: String,
+  totalPrice: {
+   type: Number,
    required: true,
-  },
-  paymentResult: {
-   id: { type: String },
-   status: { type: String },
-   update_time: { type: String },
-   email_address: { type: String },
+   default: 0.0,
   },
   isPaid: {
    type: Boolean,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
@@ -26,19 +26,6 @@ const userSchema = mongoose.Schema(
   timestamps: true,
  }
 );
-
-// userSchema.methods.matchPassword = async (enteredPassword) => {
-//  return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// userSchema.pre('save', async function (next) {
-//  if (!this.isModified('password')) {
-//   next();
-//  }
-
-//  const salt = await bcrypt.genSalt(10);
-//  this.password = await bcrypt.hash(this.password, salt);
-// });
 
 const User = mongoose.model('User', userSchema);
 
